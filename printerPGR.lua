0770b25d8ab30d099dffa8052bbb70d919d205c9
Iamaprogramer <79167125+lamaprogramer@users.noreply.github.com>
2022-11-24 19:00:59 -0500

    Add files via upload

---
+++ printerPGR.lua
@@ -0,0 +1,70 @@
+--local basalt = require("basalt")
+
+--local main = basalt.createFrame()
+
+function printpgr(main)
+
+    local printerPGR = main
+        :addButton()
+        :setText("printer")
+        :setPosition(2, 2)
+        :setSize(8, 5)
+
+    local printerInput = main
+        :addTextfield()
+        :setPosition(11, 1)
+        :setSize(32, 8)
+        :setBackground(colors.black)
+        :setForeground(colors.white)
+        :hide()
+
+    local sendInput = main
+        :addButton()
+        :setText("send")
+        :setPosition(11, 10)
+        :setSize(32, 4)
+        :hide()
+    
+    if (pocket) then
+        printerPGR:setPosition(2, 2)
+        printerInput:setPosition(11, 1)
+        sendInput:setPosition(11, 10)
+
+        printerPGR:setSize(8, 5)
+        printerInput:setSize(32, 8)
+        sendInput:setSize(32, 4)
+    else
+        printerPGR:setPosition(2, 2)
+        printerInput:setPosition(11, 1)
+        sendInput:setPosition(11, 10)
+
+        printerPGR:setSize(8, 5)
+        printerInput:setSize(32, 8)
+        sendInput:setSize(32, 4)
+    end
+
+
+    printerPGR:onClick(function(self, event, button, x, y)
+        if (event=="mouse_click")and(button==1) then
+            printerInput:show()
+            sendInput:show()
+            printerPGR:hide()
+        end
+    end)
+
+    sendInput:onClick(function(self, event, button, x, y)
+        if (event=="mouse_click")and(button==1) then
+            local printer = peripheral.find("printer")
+            local page = printer.newPage()
+
+            if page then
+                printer.write(table.concat(printerInput:getLines(), " "))
+                printer.endPage()
+            end
+        end
+    end)
+end
+
+return {printpgr = printpgr}
+
+--basalt.autoUpdate()
